2b31eb9058a02565c7a05b41c289936e4a3fc7e2
Andrea Esuli <andrea.esuli@isti.cnr.it>
2018-03-01 16:52:09 +0100

    fixed after refactor

--- class_quant.py
+++ class_quant.py
@@ -218,7 +218,7 @@ with open('hist_' + str(time.time()) + '.txt', mode='w', encoding='utf-8') as ou
             if use_teacher:
                 y_class_to_repeat = y_class
             else:
-                y_class_to_repeat = y_class_pred.squeeze(-1)
+                y_class_to_repeat = y_class_pred
             for _ in range(quant_shuffles):
                 y_class_perms.append(y_class_to_repeat.data[
                     torch.cuda.LongTensor(np.random.permutation(len(y_class)).tolist())].unsqueeze(
