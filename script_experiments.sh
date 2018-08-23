0a38212b355f94fae61e83b81533741da09cbb08
Alejandro Moreo Fernández <alejandro.moreo@isti.cnr.it>
2018-08-23 17:43:06 +0200

    last script of experiments

--- script_experiments.sh
+++ script_experiments.sh
@@ -1,29 +1,15 @@
 #!/usr/bin/env bash
 set -x
 
-#quantumconf="--stats-layer --stats-lstm"
-
-
-for run in {0..5} ; do
-for data in "kindle" "hp" "imdb" ; do
-	classmodel=../models_timing/class_net.$data.$run.pt
-	quantmodel=../models_timing/quant_net.$data.$run.pt
-	quantmodelE=../models_timing/quant_netE.$data.$run.pt
-	quantmodelS=../models_timing/quant_netS.$data.$run.pt
-	quantmodelES=../models_timing/quant_netES.$data.$run.pt
+for run in {0..9} ; do
+	for data in "kindle" "hp" "imdb" ; do
+		classmodel=../models/class_net.$data.$run.pt
+		quantmodelES=../models/quant_netES.$data.$run.pt
 	
-	python3 learn_class.py $data --output $classmodel
+		python3 learn_class.py $data --output $classmodel
 
-	python3 learn_quant.py $data $classmodel -E --stats-layer --output $quantmodelES --include-bounds
-#	python3 eval_em.py $data $classmodel $quantmodelES $run -E --plotdir ../eval_plots_formanbounds_svmkld --results ../results/formanbounds_svmkld.txt --result-note $run --include-bounds
+		python3 learn_quant.py $data $classmodel -E --stats-layer --output $quantmodelES --include-bounds
+		python3 eval_quant.py $data $classmodel $quantmodelES $run -E --plotdir ../plots --results ../results/scores.txt --result-note $run --include-bounds
 
-#	python3 learn_quant.py $data $classmodel --output $quantmodel --include-bounds
-#	python3 eval_quant.py $data $classmodel $quantmodel $run --plotdir ../eval_plots_bound --results ../results/eval_results_bound.txt --result-note $run --net-only
-
-#	python3 learn_quant.py $data $classmodel -E --output $quantmodelE --include-bounds
-#	python3 eval_quant.py $data $classmodel $quantmodelE $run -E --plotdir ../eval_plots_bound --results ../results/eval_results_bound.txt --result-note $run --net-only --include-bounds
-
-#	python3 learn_quant.py $data $classmodel --stats-layer --output $quantmodelS --include-bounds
-#	python3 eval_quant.py $data $classmodel $quantmodelS $run --plotdir ../eval_plots_bound --results ../results/eval_results_bound.txt --result-note $run --net-only --include-bounds
-done
+	done
 done
